c0143f5a39d6bc2a8f6228c8f125a85c39ca2628
Amira-badwi <83235532+Amira-badwi@users.noreply.github.com>
2023-12-29 15:19:33 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -108,3 +108,30 @@ media.forEach(e=>{
 
 })
 
+let collapse2 =document.getElementById("collapse2");
+let collapse1 =document.getElementById("collapse1");
+let collapse3 =document.getElementById("collapse3");
+let collapse4 =document.getElementById("collapse4");
+function myFunction(x) {
+   
+
+    if (x.matches) { 
+        collapse2.classList.remove("show")  ;
+        collapse1.classList.remove("show")  ;
+        collapse3.classList.remove("show")  ;
+        collapse4.classList.remove("show")  ;
+    } else {
+        collapse1.classList.add("show")  ;
+        collapse2.classList.add("show")  ;
+        collapse3.classList.add("show")  ;
+        collapse4.classList.add("show")  ;
+
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  if(collapse1) {
+
+      myFunction(x) // Call listener function at run time
+      x.addListener(myFunction)
+  }
